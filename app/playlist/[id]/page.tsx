218db2563ee0e0c10d94f178ff2bf1fcac717509
Santiago Lopez <santiagodavidlopezbenavides@gmail.com>
2024-06-23 22:14:43 -0500

    feat: add playlist page

--- app/playlist/[id]/page.tsx
+++ app/playlist/[id]/page.tsx
@@ -1,5 +1,5 @@
 import Gradient from "@/components/Gradient";
-import { playlists } from "@/lib/data";
+import { playlists, songs } from "@/lib/data";
 import { notFound } from "next/navigation";
 
 export default function PlaylistPage({ params }: { params: { id: string } }) {
@@ -9,9 +9,44 @@ export default function PlaylistPage({ params }: { params: { id: string } }) {
     notFound();
   }
 
+  const artists = playlist.artists.join(", ");
+
+  const playlistSongs = songs.filter(
+    (song) => song.albumId === playlist.albumId,
+  );
+
   return (
-    <Gradient color={playlist.color.dark}>
-      <h1 className="mb-6 text-4xl font-bold">{playlist.title}</h1>
+    <Gradient color={playlist.color.dark} className="flex flex-col">
+      <header className="mt-12 flex flex-row gap-8 px-6">
+        <picture className="aspect-square h-52 w-52 flex-none">
+          <img
+            src={playlist.cover}
+            alt={`Cover of ${playlist.title} by ${artists}`}
+            className="h-full w-full rounded-md object-cover shadow-lg"
+          />
+        </picture>
+
+        <div className="flex flex-col justify-between gap-2">
+          <h2 className="flex flex-1 items-end">Playlist</h2>
+
+          <h1 className="block text-5xl font-bold text-white">
+            {playlist.title}
+          </h1>
+
+          <div className="flex flex-1 flex-col justify-end text-sm font-normal text-gray-300">
+            <span>{playlist?.artists.join(", ")}</span>
+
+            <p className="mt-1">
+              <span className="text-white">
+                {playlistSongs.length} canciones
+              </span>
+              , 3 h aproximadamente
+            </p>
+          </div>
+        </div>
+      </header>
+
+      <div></div>
     </Gradient>
   );
 }
--- components/Gradient.tsx
+++ components/Gradient.tsx
@@ -1,9 +1,11 @@
 export default function Gradient({
   children,
   color = "",
+  className = "",
 }: {
   children: React.ReactNode;
   color?: string;
+  className?: string;
 }) {
   const style = color
     ? ({
@@ -12,7 +14,7 @@ export default function Gradient({
     : {};
 
   return (
-    <div className="relative z-10 px-6 pt-10">
+    <div className={`relative z-10 h-full px-6 pt-10 ${className}`}>
       <div
         className="absolute inset-0 -z-10 h-[calc((100vh_-_96px)/2)] bg-gradient-to-t from-neutral-900 to-neutral-600"
         style={style}
